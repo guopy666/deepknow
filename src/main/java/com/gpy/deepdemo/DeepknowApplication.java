b9deee95c7dac6410dc66400fb56d70860d27cbd
guopy <1315977102@qq.com>
2019-01-18 14:16:12 +0800

    代码自动生成配置

--- src/main/java/com/gpy/deepdemo/DeepknowApplication.java
+++ src/main/java/com/gpy/deepdemo/DeepknowApplication.java
@@ -1,9 +1,11 @@
 package com.gpy.deepdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.gpy.deepdemo.dao")
 public class DeepknowApplication {
 
 	public static void main(String[] args) {
